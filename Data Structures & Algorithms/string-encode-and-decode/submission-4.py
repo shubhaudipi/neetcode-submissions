493117f7b393d2e08cffaf353d722420e63139b7
shubhaudipi <147758013+shubhaudipi@users.noreply.github.com>
2026-08-06 23:13:37 -0400

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = []
+        for stri in strs:
+            next = str(len(stri))
+            res.append(next)
+            res.append(".")
+            res.append(stri)
+        
+        return "".join(res)
+
+    def decode(self, s: str) -> List[str]:
+        index = 0
+        res = []
+        while index < len(s):
+            numc = 0
+            while s[index+numc] != ".":
+                numc += 1
+            length = int(s[index:(index+numc)])
+            index += (numc + 1)
+            res.append(s[index:(index+length)])
+            index += length
+        
+        return res
+
+
+
+
